f688e6fb08ae55d119212aa58c1404ae72868a88
youhelin <youhelin@51creation.com>
2025-03-12 18:00:48 +0800

    fear: 轮播图组件未完成

---
+++ src/components/carousel/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="body">
+        <div class="carousel">
+            <img :src="img.src" :alt="img.alt">
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue';
+const props = defineProps({
+    carouselList: {
+        type: Array,
+        default: () => []
+    },
+
+})
+
+let img = ref({})
+let interval = ref(null)
+console.log(props.carouselList);
+
+let num = 0
+interval.value = setInterval(() => {
+    num = ++num % props.carouselList.length
+    img.value = props.carouselList[num]
+}, 3000)
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/reception/home.vue
+++ src/views/reception/home.vue
@@ -3,10 +3,14 @@
         <div class="title">
             <Menu :memuList="memuList"></Menu>
         </div>
+        <div class="carousel">
+            <Carousel :carouselList="carouselList"></Carousel>
+        </div>
     </div>
 </template>
 <script setup>
 import Menu from '@/components/menu/index.vue'
+import Carousel from '@/components/carousel/index.vue'
 import { ref } from 'vue';
 
 let memuList = ref([
@@ -15,6 +19,12 @@ let memuList = ref([
     { name: '小米澎湃OS', path: '/hyperos' },
     { name: '小米汽车', path: '/car' },
 ])
+let carouselList = ref([
+    { src: '@/assets/carsousel/carousel1.jpg', alt: 'carousel1' },
+    { src: '@/assets/carsousel/carousel2.jpg', alt: 'carousel2' },
+    { src: '@/assets/carsousel/carousel3.jpg', alt: 'carousel3' },
+])
+
 </script>
 <style lang="sass" scoped>
 
